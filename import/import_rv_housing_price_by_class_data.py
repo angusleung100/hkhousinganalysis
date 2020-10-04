381aeda4ebeffefa748740190803c7ae96b8d77f
Angus Leung <angusleung168@gmail.com>
2020-10-03 22:56:18 -0400

    Fixed filter for territories when filtering New Kowloon vs Kowloon for data set 1998 and before

--- import/import_rv_housing_price_by_class_data.py
+++ import/import_rv_housing_price_by_class_data.py
@@ -47,9 +47,13 @@ def getClassPricesForTerritory(territory, fileLoc):
     elif territory == "New Kowloon":
         filterTerritory = "New K"
     elif territory == "New Territories":
-        filterTerritory = "New"
+        filterTerritory = "New T"
     else:
         pass 
+
+
+    #Import query template
+    importBulkMonthlyPayAmountDataQuery = "INSERT IGNORE INTO `rv_price_by_class_data` (`year`,`Class_A`,`Class_B`,`Class_C`,`Class_D`,`Class_E`) VALUES "
 
     print("\n\n"+territory+"\n====")
     for year in yearColumn:
@@ -60,22 +64,42 @@ def getClassPricesForTerritory(territory, fileLoc):
                 
                 column = dataSheet[columnName]
                 if "Remarks" not in column[0]:
-                    if territory in column[0]:
+                    if filterTerritory in column[0] and filterTerritory != "Kow":   #Filter and target HK, NT, and NK
                         classification = column[0][0:7]
                         classification = classification.replace(" ","_")
 
-                            
                         print(classification)
             
-
+                    if filterTerritory in column[0] and filterTerritory == "Kow" and "New K" not in column[0]:  #Filter and target Kowloon territory
+                        classification = column[0][0:7]
+                        classification = classification.replace(" ","_")
+                            
+                        print(classification)
 
 
 
 
 #Import data from housing price by class sheet
-fileName = "../raw_data/rv_data/price_by_class_annual_86-98.csv"
+fileNameOne = "../raw_data/rv_data/price_by_class_annual_86-98.csv"
+fileNameTwo = "../raw_data/rv_data/price_by_class_annual_99-now.csv"
 
 territoryList = ["Hong Kong", "Kowloon", "New Kowloon", "New Territories"]
 
-for territorySelect in territoryList:
-    print(getClassPricesForTerritory(territorySelect, fileName))
+#Check if table exists an create if needed
+checkTableExistsQuery = cursor.execute("SHOW TABLES WHERE `Tables_in_hkhousinganalysis` = 'rv_price_by_class_data';")
+
+if checkTableExistsQuery > 0:
+    print("Table exists")
+else:
+    createPriceByClassTableQuery = "CREATE TABLE `rv_price_by_class_data` (`year` VARCHAR(255) NOT NULL, `Class_A` VARCHAR(255) NOT NULL,`Class_B` VARCHAR(255) NOT NULL,`Class_C` VARCHAR(255) NOT NULL,`Class_D` VARCHAR(255) NOT NULL,`Class_E` VARCHAR(255) NOT NULL, PRIMARY KEY (`year`));"
+    createPriceByClassTable = cursor.execute(createPriceByClassTableQuery)
+    print("RV Price By Class Table Created")
+
+#Import bulk data into database
+
+
+for territorySelect in territoryList:   #Pre-1998 data
+    getClassPricesForTerritory(territorySelect, fileNameOne)
+
+""" for territorySelect in territoryList:   #1999 onward data
+    print(getClassPricesForTerritory(territorySelect, fileNameTwo)) """
