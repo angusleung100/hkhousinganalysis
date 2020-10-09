9ddb913fe7bbe73ac379f1b5d45818fe30a0a41f
Angus Leung <angusleung168@gmail.com>
2020-10-08 23:53:55 -0400

    Pulling and displaying data from database on site index and sidebar

--- footer.php
+++ footer.php
@@ -26,22 +26,87 @@
                             <h4>Monthly Hang Seng Index (HKD)</h4>
                             
                         </div>
+
+                        <?php
+                            $getUnitsSoldDataQuery = mysqli_query($connection, "SELECT * FROM `rv_units_sold_all_territories_data` ORDER BY `date` DESC;");
+                            $getUnitsSoldData = mysqli_fetch_all($getUnitsSoldDataQuery);
+
+                            $combinedUnitsSold = $getUnitsSoldData[0][1] + $getUnitsSoldData[0][3];
+
+                            $combinedUnitsSoldPreviousMonth = $getUnitsSoldData[1][1] + $getUnitsSoldData[1][3];
+                        ?>
+
                         <div class="section">
-                            <h1>100</h1>
-                            <h5>+10%</h5>
-                            <h4>Units Sold (YTD)</h4>
+                            <h1><?php echo $combinedUnitsSold; ?></h1>
+                            <h5>
+                            <?php
+
+                                $deltaChange = $combinedUnitsSold - $combinedUnitsSoldPreviousMonth;
+
+                                $symbol = "";
+
+                                if($deltaChange >= 0)
+                                {
+                                    $symbol = "+";
+                                }
+                                echo $symbol . $deltaChange;
+                            ?>
+                             units</h5>
+                            <h4>Units Sold (vs prev month)</h4>
                             
                         </div>
+
+                        <?php
+                            //Get housing index data
+                            $getPricingIndexDataQuery = mysqli_query($connection, "SELECT * FROM `rv_price_index_by_class_data` ORDER BY `year` DESC;");
+
+                            $getPricingIndexData = mysqli_fetch_all($getPricingIndexDataQuery);
+                        ?>
                         <div class="section">
-                            <h1>100</h1>
-                            <h5>+10%</h5>
-                            <h4>Housing Index (YTD)</h4>
+                            <h1>
+                            <?php echo round($getPricingIndexData[0][6], 2) ?>
+                            </h1>
+                            <h5>
+                            <?php
+
+                                $deltaChange = ($getPricingIndexData[0][6] - $getPricingIndexData[1][6]);
+
+                                $symbol = "";
+
+                                if($deltaChange >= 0)
+                                {
+                                    $symbol = "+";
+                                }
+                                echo $symbol . round(($deltaChange / $getPricingIndexData[0][6]) * 100, 2) . "% (" . round($deltaChange, 2) .  " pts)";
+                            ?>
+                            </h5>
+                            <h4>Housing Index (vs prev month)</h4>
                             
                         </div>
+
+                        <?php
+                            //Get renting index data
+                            $getRentingIndexDataQuery = mysqli_query($connection, "SELECT * FROM `rv_rent_index_by_class_data` ORDER BY `year` DESC;");
+
+                            $getRentingIndexData = mysqli_fetch_all($getRentingIndexDataQuery);
+                        ?>
                         <div class="section">
-                            <h1>100</h1>
-                            <h5>+10%</h5>
-                            <h4>Rental Index (YTD)</h4>
+                            <h1><?php echo round($getRentingIndexData[0][6], 2) ?></h1>
+                            <h5>
+                            <?php
+
+                                $deltaChange = ($getRentingIndexData[0][6] - $getRentingIndexData[1][6]);
+
+                                $symbol = "";
+
+                                if($deltaChange >= 0)
+                                {
+                                    $symbol = "+";
+                                }
+                                echo $symbol . round(($deltaChange / $getRentingIndexData[0][6]) * 100, 2) . "% (" . round($deltaChange, 2) .  " pts)";
+                            ?>
+                            </h5>
+                            <h4>Rental Index (vs prev month)</h4>
                             
                         </div>
                        
@@ -52,9 +117,12 @@
                         </div>
                         <div class="section">
                             <small>
+                                Data displayed and provided are either monthly or yearly data, not necessarily up-to-date for the current month. HK Housing Analysis will note what time interval is currently being displayed for data, as well as what date is the data shown for.
+                                <br><br>
                                 Data by third-party providers is not guaranteed to be fully accurate and should only be used as reference and not financial advice whatsoever.
                                 Please refer to a real estate agent, financial advisor, or accredited person(s) on your current financial situation. HK Housing Analysis is not liable or responsible
                                 for financial or legal damages resulting in the use of the data provided.
+                                
                             </small>
                         </div>
                         
--- index.php
+++ index.php
@@ -3,40 +3,319 @@
 ?>
 
 <?php include("header.php"); ?>
+
+<h3>Price Averages</h3>
+<table>
+    <thead>
+        <tr>
+            <th>Region</th>
+            <th>Price per sq ft (HKD)</th>
+            <th>Change from previous year (%)</th>
+        </tr>
+    </thead>
+    <tbody>
+
+    <?php
+        //Calculate all territories average
+        
+        $hkiAverage = 0;
+        $kowAverage = 0;
+        $ntAverage = 0;
+
+        $hkiPreviousAverage = 0;
+        $kowPreviousAverage = 0;
+        $ntPreviousAverage = 0;
+
+    ?>
+    <?php 
+
+        //Get housing price data HONG KONG ISLAND
+
+        $getHKIPricesDataQuery = mysqli_query($connection, "SELECT * FROM `rv_price_by_class_data_hki` ORDER BY `year` DESC;");
+
+        $getHKIPricesData = mysqli_fetch_all($getHKIPricesDataQuery);
+
+        //var_dump($getHKIPricesData);
+
+        $average = 0;
+
+        for($i=0; $i < 6; $i++)
+        {
+            $average += $getHKIPricesData[0][$i];
+        }
+        $average = $average / 5;
+
+        $previousYearAverage = 0;
+
+        for($i=0; $i < 6; $i++)
+        {
+            $previousYearAverage += $getHKIPricesData[1][$i];
+        }
+        $previousYearAverage = $previousYearAverage / 5;
+
+        $deltaChange = $average - $previousYearAverage;
+
+        $symbol = "";
+
+        if($deltaChange >= 0)
+        {
+            $symbol = "+";
+        }
+
+        $hkiAverage = $average;
+        $hkiPreviousAverage = $previousYearAverage;
+    ?>
+    <tr>
+        <td>Hong Kong</td>
+        <td>$<?php echo $average; ?></td>
+        <td><?php echo $symbol . round($deltaChange / $previousYearAverage, 2) . "% ($" . $deltaChange . ")"; ?></td>
+    </tr>
+
+    
+    <?php 
+
+        //Get housing price data KOWLOON
+
+        $getKOWPricesDataQuery = mysqli_query($connection, "SELECT * FROM `rv_price_by_class_data_kow` ORDER BY `year` DESC;");
+
+        $getKOWPricesData = mysqli_fetch_all($getKOWPricesDataQuery);
+
+        //var_dump($getKOWPricesData);
+
+        $average = 0;
+
+        for($i=0; $i < 6; $i++)
+        {
+            $average += $getKOWPricesData[0][$i];
+        }
+        $average = $average / 5;
+
+        $previousYearAverage = 0;
+
+        for($i=0; $i < 6; $i++)
+        {
+            $previousYearAverage += $getKOWPricesData[1][$i];
+        }
+        $previousYearAverage = $previousYearAverage / 5;
+
+        $deltaChange = $average - $previousYearAverage;
+
+        $symbol = "";
+
+        if($deltaChange >= 0)
+        {
+            $symbol = "+";
+        }
+
+        $kowAverage = $average;
+        $kowPreviousAverage = $previousYearAverage;
+    ?>
+    <tr>
+        <td>Kowloon</td>
+        <td>$<?php echo $average; ?></td>
+        <td><?php echo $symbol . round($deltaChange / $previousYearAverage, 2) . "% ($" . $deltaChange . ")"; ?></td>
+    </tr>
+
+    
+    <?php 
+
+        //Get housing price data NEW TERRITORIES
+
+        $getntPricesDataQuery = mysqli_query($connection, "SELECT * FROM `rv_price_by_class_data_nt` ORDER BY `year` DESC;");
+
+        $getntPricesData = mysqli_fetch_all($getntPricesDataQuery);
+
+        //var_dump($getHKIPricesData);
+
+        $average = 0;
+
+        for($i=0; $i < 6; $i++)
+        {
+            $average += $getntPricesData[0][$i];
+        }
+        $average = $average / 5;
+
+        $previousYearAverage = 0;
+
+        for($i=0; $i < 6; $i++)
+        {
+            $previousYearAverage += $getntPricesData[1][$i];
+        }
+        $previousYearAverage = $previousYearAverage / 5;
+
+        $deltaChange = $average - $previousYearAverage;
+
+        $symbol = "";
+
+        if($deltaChange >= 0)
+        {
+            $symbol = "+";
+        }
+
+        $ntAverage = $average;
+        $ntPreviousAverage = $previousYearAverage;
+    ?>
+    <tr>
+        <td>New Territories</td>
+        <td>$<?php echo $average; ?></td>
+        <td><?php echo $symbol . round($deltaChange / $previousYearAverage, 2) . "% ($" . $deltaChange . ")"; ?></td>
+    </tr>
+
+    
+    <?php 
+
+        //Get housing price data ALL
+
+        $average = $hkiAverage + $kowAverage + $ntAverage;
+        $average = $average / 5;
+
+        $previousYearAverage = $hkiPreviousAverage + $kowPreviousAverage + $ntPreviousAverage;
+
+        $previousYearAverage = $previousYearAverage / 5;
+
+        $deltaChange = $average - $previousYearAverage;
+
+        $symbol = "";
+
+        if($deltaChange >= 0)
+        {
+            $symbol = "+";
+        }
+
+    ?>
+    <tr>
+        <td><b>All Territories</b></td>
+        <td>$<?php echo $average; ?></td>
+        <td><?php echo $symbol . round($deltaChange / $previousYearAverage, 2) . "% ($" . $deltaChange . ")"; ?></td>
+    </tr>
+    </tbody>
+</table>
+
+
 <h3>Housing Indices</h3>
 <table>
     <thead>
         <tr>
             <th>Grade</th>
-            <th>Price per sq ft (HKD)</th>
-            <th>YTD (%)</th>
-            <th>Overall (%)</th>
+            <th>Index (Pts)</th>
+            <th>Change from previous year (%)</th>
         </tr>
     </thead>
     <tbody>
+
+    <?php
+        //Get housing index data
+        $getPricingIndexDataQuery = mysqli_query($connection, "SELECT * FROM `rv_price_index_by_class_data` ORDER BY `year` DESC;");
+
+        $getPricingIndexData = mysqli_fetch_all($getPricingIndexDataQuery);
+    ?>
     <tr>
         <td>A</td>
-        <td></td>
-        <td></td>
-        <td></td>
+        <td><?php echo round($getPricingIndexData[0][1], 2) ?></td>
+        <td>
+            <?php
+
+                $deltaChange = ($getPricingIndexData[0][1] - $getPricingIndexData[1][1]);
+
+                $symbol = "";
+
+                if($deltaChange >= 0)
+                {
+                    $symbol = "+";
+                }
+                echo $symbol . round(($deltaChange / $getPricingIndexData[0][1]) * 100, 2) . "% (" . round($deltaChange, 2) .  " pts)";
+            ?>
+        </td>
     </tr>
     <tr>
         <td>B</td>
-        <td></td>
-        <td></td>
-        <td></td>
+        <td><?php echo round($getPricingIndexData[0][2], 2) ?></td>
+        <td>
+            <?php
+
+                $deltaChange = ($getPricingIndexData[0][2] - $getPricingIndexData[1][2]);
+
+                $symbol = "";
+
+                if($deltaChange >= 0)
+                {
+                    $symbol = "+";
+                }
+                echo $symbol . round(($deltaChange / $getPricingIndexData[0][2]) * 100, 2) . "% (" . round($deltaChange, 2) .  " pts)";
+            ?>
+        </td>
     </tr>
     <tr>
         <td>C</td>
-        <td></td>
-        <td></td>
-        <td></td>
+        <td><?php echo round($getPricingIndexData[0][3], 2) ?></td>
+        <td>
+            <?php
+
+                $deltaChange = ($getPricingIndexData[0][3] - $getPricingIndexData[1][3]);
+
+                $symbol = "";
+
+                if($deltaChange >= 0)
+                {
+                    $symbol = "+";
+                }
+                echo $symbol . round(($deltaChange / $getPricingIndexData[0][3]) * 100, 2) . "% (" . round($deltaChange, 2) .  " pts)";
+            ?>
+        </td>
     </tr>
     <tr>
-        <td><b>Consolidated Index</b></td>
-        <td><b></b></td>
-        <td><b></b></td>
-        <td><b></b></td>
+        <td>D</td>
+        <td><?php echo round($getPricingIndexData[0][4], 2) ?></td>
+        <td>
+            <?php
+
+                $deltaChange = ($getPricingIndexData[0][4] - $getPricingIndexData[1][4]);
+
+                $symbol = "";
+
+                if($deltaChange >= 0)
+                {
+                    $symbol = "+";
+                }
+                echo $symbol . round(($deltaChange / $getPricingIndexData[0][4]) * 100, 2) . "% (" . round($deltaChange, 2) .  " pts)";
+            ?>
+        </td>
+    </tr>
+    <tr>
+        <td>E</td>
+        <td><?php echo round($getPricingIndexData[0][5], 2) ?></td>
+        <td>
+            <?php
+
+                $deltaChange = ($getPricingIndexData[0][5] - $getPricingIndexData[1][5]);
+
+                $symbol = "";
+
+                if($deltaChange >= 0)
+                {
+                    $symbol = "+";
+                }
+                echo $symbol . round(($deltaChange / $getPricingIndexData[0][5]) * 100, 2) . "% (" . round($deltaChange, 2) .  " pts)";
+            ?>
+        </td>
+    </tr>
+    <tr>
+        <td><b>All Grades</b></td>
+        <td><b><?php echo round($getPricingIndexData[0][6], 2) ?></b></td>
+        <td><b>
+            <?php
+
+                $deltaChange = ($getPricingIndexData[0][6] - $getPricingIndexData[1][6]);
+
+                $symbol = "";
+
+                if($deltaChange >= 0)
+                {
+                    $symbol = "+";
+                }
+                echo $symbol . round(($deltaChange / $getPricingIndexData[0][6]) * 100, 2) . "% (" . round($deltaChange, 2) .  " pts)";
+            ?>
+        </b></td>
     </tr>
     </tbody>
 </table>
@@ -46,80 +325,129 @@
     <thead>
         <tr>
             <th>Grade</th>
-            <th>Price per sq ft (HKD)</th>
-            <th>YTD (%)</th>
-            <th>Overall (%)</th>
+            <th>Index (Pts)</th>
+            <th>Change from previous year (%)</th>
         </tr>
     </thead>
     <tbody>
+
+    <?php
+        //Get renting index data
+        $getRentingIndexDataQuery = mysqli_query($connection, "SELECT * FROM `rv_rent_index_by_class_data` ORDER BY `year` DESC;");
+
+        $getRentingIndexData = mysqli_fetch_all($getRentingIndexDataQuery);
+    ?>
     <tr>
         <td>A</td>
-        <td></td>
-        <td></td>
-        <td></td>
+        <td><?php echo round($getRentingIndexData[0][1], 2) ?></td>
+        <td>
+            <?php
+
+                $deltaChange = ($getRentingIndexData[0][1] - $getRentingIndexData[1][1]);
+
+                $symbol = "";
+
+                if($deltaChange >= 0)
+                {
+                    $symbol = "+";
+                }
+                echo $symbol . round(($deltaChange / $getRentingIndexData[0][1]) * 100, 2) . "% (" . round($deltaChange, 2) .  " pts)";
+            ?>
+        </td>
     </tr>
     <tr>
         <td>B</td>
-        <td></td>
-        <td></td>
-        <td></td>
+        <td><?php echo round($getRentingIndexData[0][2], 2) ?></td>
+        <td>
+            <?php
+
+                $deltaChange = ($getRentingIndexData[0][2] - $getRentingIndexData[1][2]);
+
+                $symbol = "";
+
+                if($deltaChange >= 0)
+                {
+                    $symbol = "+";
+                }
+                echo $symbol . round(($deltaChange / $getRentingIndexData[0][2]) * 100, 2) . "% (" . round($deltaChange, 2) .  " pts)";
+            ?>
+        </td>
     </tr>
     <tr>
         <td>C</td>
-        <td></td>
-        <td></td>
-        <td></td>
+        <td><?php echo round($getRentingIndexData[0][3], 2) ?></td>
+        <td>
+            <?php
+
+                $deltaChange = ($getRentingIndexData[0][3] - $getRentingIndexData[1][3]);
+
+                $symbol = "";
+
+                if($deltaChange >= 0)
+                {
+                    $symbol = "+";
+                }
+                echo $symbol . round(($deltaChange / $getRentingIndexData[0][3]) * 100, 2) . "% (" . round($deltaChange, 2) .  " pts)";
+            ?>
+        </td>
     </tr>
     <tr>
-        <td><b>Consolidated Index</b></td>
-        <td><b></b></td>
-        <td><b></b></td>
-        <td><b></b></td>
+        <td>D</td>
+        <td><?php echo round($getRentingIndexData[0][4], 2) ?></td>
+        <td>
+            <?php
+
+                $deltaChange = ($getRentingIndexData[0][4] - $getRentingIndexData[1][4]);
+
+                $symbol = "";
+
+                if($deltaChange >= 0)
+                {
+                    $symbol = "+";
+                }
+                echo $symbol . round(($deltaChange / $getRentingIndexData[0][4]) * 100, 2) . "% (" . round($deltaChange, 2) .  " pts)";
+            ?>
+        </td>
+    </tr>
+    <tr>
+        <td>E</td>
+        <td><?php echo round($getRentingIndexData[0][5], 2) ?></td>
+        <td>
+            <?php
+
+                $deltaChange = ($getRentingIndexData[0][5] - $getRentingIndexData[1][5]);
+
+                $symbol = "";
+
+                if($deltaChange >= 0)
+                {
+                    $symbol = "+";
+                }
+                echo $symbol . round(($deltaChange / $getRentingIndexData[0][5]) * 100, 2) . "% (" . round($deltaChange, 2) .  " pts)";
+            ?>
+        </td>
+    </tr>
+    <tr>
+        <td><b>All Grades</b></td>
+        <td><b><?php echo round($getRentingIndexData[0][6], 2) ?></b></td>
+        <td><b>
+            <?php
+
+                $deltaChange = ($getRentingIndexData[0][6] - $getRentingIndexData[1][6]);
+
+                $symbol = "";
+
+                if($deltaChange >= 0)
+                {
+                    $symbol = "+";
+                }
+                echo $symbol . round(($deltaChange / $getRentingIndexData[0][6]) * 100, 2) . "% (" . round($deltaChange, 2) .  " pts)";
+            ?>
+        </b></td>
     </tr>
     </tbody>
 </table>
 
-<h3>Prices</h3>
-<table>
-    <thead>
-        <tr>
-            <th>Region</th>
-            <th>Price per sq ft (HKD)</th>
-            <th>YTD (%)</th>
-            <th>Overall (%)</th>
-            <th>Units Sold</th>
-        </tr>
-    </thead>
-    <tbody>
-    <tr>
-        <td>Hong Kong</td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-    </tr>
-    <tr>
-        <td>Kowloon</td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-    </tr>
-    <tr>
-        <td>New Territories</td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-    </tr>
-    <tr>
-        <td><b>Consolidated Index</b></td>
-        <td><b></b></td>
-        <td><b></b></td>
-        <td><b></b></td>
-        <td><b></b></td>
-    </tr>
-    </tbody>
-</table>
+
 
 <?php include("footer.php"); ?>
